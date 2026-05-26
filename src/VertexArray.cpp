17245d83f407b7baa57d93de82ececb311d2e00e
Clément <welsch.clement@gmail.com>
2026-05-26 19:52:59 +0200

    add a gradient coloring background

--- src/VertexArray.cpp
+++ src/VertexArray.cpp
@@ -17,14 +17,27 @@ void VertexArray::AddBuffer(const VertexBuffer& _vb, const VertexBufferLayout& l
 	Bind();
 	_vb.Bind();
 	const std::vector<VertexBufferElement>& elements = layout.GetElements();
-	unsigned int offest = 0;
+	unsigned int offset = 0;
 
 	for (unsigned int i = 0; i < elements.size(); i++)
 	{
-		const VertexBufferElement& element = elements[i];
-		GLCall(glEnableVertexAttribArray(0));
-		GLCall(glVertexAttribPointer(i, element.m_count, element.m_type, element.m_normalized, layout.GetStride(), (const void*) offest));
-		offest += element.m_count * VertexBufferElement::GetSizeOfType(element.m_type);
+		auto& element = elements[i];
+
+		GLCall(glEnableVertexAttribArray(i));
+
+		GLCall(glVertexAttribPointer(
+			i,
+			element.m_count,
+			element.m_type,
+			element.m_normalized,
+			layout.GetStride(),
+			(const void*)offset
+		));
+
+		/*glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));*/
+
+		offset += element.m_count * VertexBufferElement::GetSizeOfType(element.m_type);
 	}
 }
 
